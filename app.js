3c5edc4e2c085657c76f6aeec5eeae02aa90ae82
acfoard <acfoard@gmail.com>
2019-04-20 16:33:35 -0400

    form submit functionality

--- app.js
+++ app.js
@@ -1,31 +1,56 @@
 // debugger;
 
 const hideAll = function() {
-    $(".contentPage").addClass('hide');
+  $(".contentPage").addClass("hide");
 };
 
 const showAbout = function(e) {
-    if (e) {
-        e.preventDefault();
-    } 
-    hideAll();
-    $("#home").removeClass('hide');
+  if (e) {
+    e.preventDefault();
+  }
+  hideAll();
+  $("#home").removeClass("hide");
 };
 
 const showPortfolio = function(e) {
-    e.preventDefault();
-    hideAll();
-    $("#portfolioPage").removeClass('hide');
+  e.preventDefault();
+  hideAll();
+  $("#portfolioPage").removeClass("hide");
 };
 
 const showContact = function(e) {
-    e.preventDefault();
-    hideAll();
-    $("#contactPage").removeClass('hide');
+  e.preventDefault();
+  hideAll();
+  $("#contactPage").removeClass("hide");
 };
 
 showAbout();
 
-$('#showHome').on('click', showAbout);
-$('#showPortfolio').on('click', showPortfolio);
-$('#showContact').on('click', showContact);
+$("#showHome").on("click", showAbout);
+$("#showPortfolio").on("click", showPortfolio);
+$("#showContact").on("click", showContact);
+
+const sendEmail = function(e) {
+  e.preventDefault();
+  const fromName = $("#fromName").val();
+  const fromEmail = $("#fromEmail").val();
+  const message = $("#message").val();
+  const params = {
+    fromName: fromName,
+    fromEmail: fromEmail,
+    message: message
+  };
+  emailjs.send("gmail", "formsubmit", params).then(
+    function(response) {
+      $("#fromName").val('');
+      $("#fromEmail").val('');
+      $("#message").val('');
+      console.log("Success", response.status, response.text);
+    },
+    function(error) {
+      console.log("Error:", error);
+    }
+  );
+};
+
+$("#submit").on("click", sendEmail);
